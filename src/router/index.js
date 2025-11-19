86755539f1ce9865760060aade16400937d38078
Aleksey Ignatyev <a.y.ignatyev@sdventures.com>
2025-11-19 09:29:55 +0100

    switch router history mode from createWebHistory to createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Declension from '../views/Declension.vue'
 import Pronouns from '../views/Pronouns.vue'
 import Numerals from '../views/Numerals.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
